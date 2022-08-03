93de9c5f270dff0c3569644514739940cd6b7d85
Alfarabotz567 <109883756+Alfarabotz567@users.noreply.github.com>
2022-08-03 14:18:25 +0700

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -39,7 +39,7 @@ let handler  = async (m, { conn, args, usedPrefix, command }) => {
       parent.reply(m.chat, 'Berhasil tersambung dengan WhatsApp - mu.\n*NOTE: Ini cuma numpang*\n' + JSON.stringify(user, null, 2), m)
       if (auth) return
       await parent.sendMessage(user.jid, `Kamu bisa login tanpa qr dengan pesan dibawah ini. untuk mendapatkan kode lengkapnya, silahkan kirim *${usedPrefix}getcode* untuk mendapatkan kode yang akurat`, MessageType.extendedText)
-      parent.sendMessage(user.jid, `${usedPrefix + command} ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, MessageType.extendedText)
+      conn.sendMessage(user.jid, `${usedPrefix + command} ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, MessageType.extendedText)
     })
     setTimeout(() => {
       if (conn.user) return
